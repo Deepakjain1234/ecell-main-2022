5de4b10771409c7c865851e7bdfc2b68271f6d10
shrish-nitb <shrish108@gmail.com>
2022-07-06 17:15:05 +0530

    shrish commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
-
 gsap.to("body", {
     duration: 2,
-    "background-image": "linear-gradient(315deg, #130f40 0%, #000000 65%),url(./assets/images/noise-fine.svg)"
+    "background-image":
+        "linear-gradient(315deg, #130f40 0%, #000000 65%),url(./assets/images/noise-fine.svg)",
 });
 setTimeout(function () {
-    document.body.style.background = "linear-gradient(315deg, #130f40 0%, #000000 65%)"
+    document.body.style.background =
+        "linear-gradient(315deg, #130f40 0%, #000000 65%)";
 }, 2100);
-
 
 // particlesJS("particles-js", {
 //     "particles": {
@@ -114,7 +114,7 @@ setTimeout(function () {
 //             "remove": {
 //                 "particles_nb": 2
 //             }
-//         }s
+//         }
 //     },
 //     "retina_detect": true
 // });
@@ -137,24 +137,19 @@ setTimeout(function () {
 // };
 // requestAnimationFrame(update);
 
-
 // document.getElementsByTagName('canvas')[1].style.display = "none";
 var tl;
+var transition = { playing: true };
 function textEffect1(textId) {
     var a = document.getElementById(textId).innerText;
     var b = a.split("");
     for (var l in b) {
         b[l] =
-            "<div class='shadowInherit'>" +
-            b[l].replace(" ", "&nbsp;") +
-            "</div>";
+            "<div class='shadowInherit'>" + b[l].replace(" ", "&nbsp;") + "</div>";
     }
-    document.getElementById(textId).innerHTML =
-        "<div>" + b.join("") + "</div>";
+    document.getElementById(textId).innerHTML = "<div>" + b.join("") + "</div>";
 
-    var mySplitText = document
-        .getElementById(textId)
-        .getElementsByTagName("div"),
+    var mySplitText = document.getElementById(textId).getElementsByTagName("div"),
         numChars = mySplitText.length;
     tl = new TimelineLite();
     for (var i = 0; i < numChars; i++) {
@@ -163,158 +158,198 @@ function textEffect1(textId) {
         }
         // mySplitText[i].style.opacity = 0.7 + Math.random();
         //random value used as position parameter
-        tl.from(mySplitText[i], 2, {
-            opacity: 0
-        }, Math.random() * 2);
+        tl
+        .from(
+            mySplitText[i],
+            2,
+            {
+                opacity: 0,
+            },
+            Math.random() * 2
+        )
+        ;
     }
 }
 
 textEffect1("heading");
 textEffect1("subHeading");
 
-
-
 var noise = "./assets/images/noise.svg";
 let tl2 = gsap.timeline({
     defaults: {
-        ease: "power4.inOut"
+        ease: "power4.inOut",
     },
-    paused: true
+    paused: true,
 });
-tl2.to(
-        "#first", {
+tl2
+    .to("#first", {
         height: "0vh",
         duration: 2,
-    }
-    )
+    })
     .to(
-        "#second", {
-        height: "100vh",
-        duration: 2,
-    },
+        "#second",
+        {
+            height: "100vh",
+            duration: 2,
+        },
         "<"
     )
     .to(
-        "#camel", {
-        fill: "#ffffff29",
-        duration: 1,
-    },
+        "#camel",
+        {
+            fill: "#ffffff29",
+            duration: 1,
+        },
         "<"
     )
     .to(
-        "#about-head", {
-        "clip-path": "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-        opacity: 1,
-        y: 0,
-        duration: 2,
-    },
+        "#about-head",
+        {
+            "clip-path": "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+            opacity: 1,
+            y: 0,
+            duration: 2,
+        },
         "<0.8"
     )
     .to(
-        ".about-desc", {
-        y: 0,
-        color: "rgb(255, 255, 255, 90%)",
-        opacity: 0.8,
-        duration: 2.3,
-    },
+        ".about-desc",
+        {
+            transform: "translateY(0px) scale(0.9)",
+            color: "rgb(255, 255, 255, 90%)",
+            opacity: 0.9,
+            duration: 2.3,
+        },
         "<"
     )
     .to(
-        ".about-desc", {
-        opacity: 0.9,
-        duration: 3,
-    },
+        "#about-vid",
+        {
+            opacity: 1,
+            duration: 3,
+        },
         "<"
     )
     .to(
-        "#about-vid", {
-        opacity: 1,
-        duration: 3,
-    },
-        "<"
-    )
-    .to(
-        "body", {
-        "background-color": "#000000",
-        background: "linear-gradient(147deg, rgb(0, 0, 0) 0%, rgb(50 2 41) 74%), url(./assets/images/noise.svg) rgb(0, 0, 0)",
-        height: "100vh",
-        duration: 1,
-    },
+        "body",
+        {
+            "background-color": "#000000",
+            background:
+                "linear-gradient(147deg, rgb(0, 0, 0) 0%, rgb(50 2 41) 74%), url(./assets/images/noise.svg) rgb(0, 0, 0)",
+            height: "100vh",
+            duration: 1,
+        },
         "<"
     )
     // linear-gradient(147deg, rgb(0, 0, 0) 0%, rgb(4, 97, 159) 74%), url("./assets/images/noise.svg") rgb(0, 0, 0)
     .to(
-        "canvas", {
-        height: "0px",
-        duration: 1,
-    },
+        "canvas",
+        {
+            height: "0px",
+            duration: 1,
+        },
         "<"
     )
     .to(
-        "#logo", {
-        filter: "",
-        duration: 0.2,
-    },
+        "#logo",
+        {
+            filter: "",
+            duration: 0.2,
+        },
         "<"
     )
     .to(
-        ".particles-js-canvas-el", {
-        height: "100vh",
-        opacity: 0.75,
-        duration: 2,
-    },
+        ".particles-js-canvas-el",
+        {
+            height: "100vh",
+            opacity: 0.75,
+            duration: 2,
+        },
         "<0.05"
-    );
-    
+    )
+    .to(
+        "#logo",
+        {
+            filter: "",
+            duration: 0.2,
+        },
+        "<"
+    )
+    .to(transition, {
+        playing: false,
+    });
+
 let tl3 = gsap.timeline({
     defaults: {
-        ease: "power4.inOut"
+        ease: "power4.inOut",
     },
-    paused: true
-})
-tl3.to(
-        "#second", {
+    paused: true,
+});
+tl3
+    .to("#second", {
         height: "0",
         duration: 2,
-    }
+    })
+    .to(
+        "#third",
+        {
+            height: "100vh",
+            duration: 2,
+        },
+        "<"
     )
     .to(
-        "#third", {
-        height: "100vh",
-        duration: 2,
-    }, "<"
-    )
-    .to(
-        "body", {
-        height: "100vh",
-        duration: 3,
-    },
+        "body",
+        {
+            height: "100vh",
+            duration: 3,
+        },
         "<+1"
-    );
+    )
+    .to(transition, {
+        playing: false,
+    });
 
 let tl4 = gsap.timeline({
     defaults: {
-        ease: "power4.inOut"
+        ease: "power4.inOut",
     },
-    paused: true
-})
-tl4.to("#third", {
-    height: "0",
-    y: "-100vh",
-    duration: 2,
-})
+    paused: true,
+});
+tl4
+    .to("#third", {
+        height: "0",
+        y: "-100vh",
+        duration: 2,
+    })
     .to(
-        "body", {
-        height: "100vh",
-        duration: 3,
-    },
+        "#fourth",
+        {
+            height: "100vh",
+            duration: 2,
+        },
+        "<"
+    )
+    .to(
+        "body",
+        {
+            height: "100vh",
+            duration: 3,
+        },
         "<+1"
-    );
+    )
+    .to(transition, {
+        playing: false,
+    });
 
 // tl2.reverse(0);
 var currentPage = 1;
+setTimeout(function(){
+    transition.playing = false;
+}, 5000)
 window.addEventListener("wheel", function (e) {
-    if (e.wheelDelta < -90 && !tl4.isActive() && !tl3.isActive() && !tl2.isActive() && !tl.isActive()) {
+    if (e.wheelDelta < -5 && !transition.playing) {
+        transition.playing = true;
         if (currentPage == 1) {
             tl2.play();
             currentPage += 1;
@@ -326,8 +361,7 @@ window.addEventListener("wheel", function (e) {
             currentPage += 1;
         }
     }
-})
-
+});
 
 //check for Navigation Timing API support
 if (window.performance) {
@@ -335,7 +369,6 @@ if (window.performance) {
 }
 console.info(performance.navigation.type);
 if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
-   
 } else {
-    console.info( "This page is not reloaded");
+    console.info("This page is not reloaded");
 }
