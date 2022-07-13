aed6046ccdd1f3b6b25711fcdccd13df59c834a5
shrish-nitb <shrish108@gmail.com>
2022-07-13 05:57:25 +0530

    shrish commit

---
+++ assets/js/cards.js
@@ -0,0 +1,470 @@
+var a = 3;
+//   IMPORTING
+import * as THREE from "three";
+
+import { TWEEN } from "./jsm/libs/tween.module.min.js";
+//   import { TrackballControls } from "./jsm/controls/TrackballControls.js";
+import { CSS3DRenderer, CSS3DObject } from "./jsm/renderers/CSS3DRenderer.js";
+//   /IMPORTING
+
+const table = [
+  "speaker1",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "speaker2",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "speaker3",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "speaker4",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "speaker5",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "speaker6",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "speaker7",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "speaker8",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "speaker9",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "speaker10",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "speaker11",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "speaker12",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "speaker13",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "speaker14",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "speaker15",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "speaker16",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "speaker17",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "speaker18",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z2speaker1",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z2speaker2",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z2speaker3",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z2speaker4",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z2speaker5",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z2speaker6",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z2speaker7",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z2speaker8",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z2speaker9",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z2speaker10",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z2speaker11",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z2speaker12",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z2speaker13",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z2speaker14",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z2speaker15",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z2speaker16",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z2speaker17",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z2speaker18",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z3speaker1",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z3speaker2",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z3speaker3",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z3speaker4",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z3speaker5",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z3speaker6",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z3speaker7",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z3speaker8",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z3speaker9",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z3speaker10",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z3speaker11",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z3speaker12",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z3speaker13",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z3speaker14",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z3speaker15",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z3speaker16",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z3speaker17",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+  "Z3speaker18",
+  "Organization1",
+  "Position1",
+  1,
+  1,
+];
+
+console.log(table.length / 5);
+let camera, scene, renderer;
+//   let controls;
+
+const objects = [];
+const targets = { grid: [] };
+
+init();
+animate();
+
+function init() {
+  // init starts
+  camera = new THREE.PerspectiveCamera(
+    40,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
+  );
+  camera.position.z = 0;
+
+  scene = new THREE.Scene();
+
+  // table
+
+  for (let i = 0; i < table.length; i += 5) {
+    const element = document.createElement("div");
+    var bgc = "rgba(0,127,127," + (Math.random() * 0.5 + 0.25) + ")";
+    element.className = "element";
+    element.style.backgroundColor = bgc;
+
+    const cardPic = document.createElement("div");
+    cardPic.className = "cardPic";
+    cardPic.style.background =
+      "url('https://upload.wikimedia.org/wikipedia/commons/f/fe/Mark_Zuckerberg_em_setembro_de_2014.jpg')";
+    element.appendChild(cardPic);
+
+    const overlay = document.createElement("div");
+    overlay.className = "overlay";
+    element.appendChild(overlay);
+
+    const cardName = document.createElement("div");
+    cardName.className = "cardName";
+    cardName.textContent = table[i];
+    element.appendChild(cardName);
+
+    const details = document.createElement("div");
+    details.className = "details";
+    details.innerHTML = table[i + 1] + "<br>" + table[i + 2];
+    element.appendChild(details);
+
+    const objectCSS = new CSS3DObject(element);
+    objectCSS.position.x = Math.random() * 4000 - 2000;
+    objectCSS.position.y = Math.random() * 4000 - 2000;
+    objectCSS.position.z = Math.random() * 4000 - 2000;
+    scene.add(objectCSS);
+
+    objects.push(objectCSS);
+  }
+
+  // grid MAKER
+
+  for (let i = 0; i < objects.length; i++) {
+    const object = new THREE.Object3D();
+    const rows = 3;
+    const cols = 6;
+    const gapX = 130; //in pixels
+    const gapY = 130; //in pixels
+    const gapZ = 520;
+
+    //centering logic applied
+    const gridLeft =
+      (innerWidth * 2 - 100) / 2 - (cols * 120 + (cols - 1) * gapX) / 2; //innerWidth * 2 -100 right edge
+    const gridTop =
+      (innerHeight * 2 - 35) / 2 - (rows * 160 + (rows - 1) * gapY) / 2; //innerHeight * 2 - 35 bottom edge
+
+    object.position.x = (i % cols) * (120 + gapX) - innerWidth + 100 + gridLeft;
+
+    object.position.y =
+      -(Math.floor(i / cols) % rows) * (160 + gapY) +
+      innerHeight -
+      100 -
+      gridTop;
+
+    object.position.z = Math.floor(i / 18) * gapZ - 2000;
+
+    targets.grid.push(object);
+  }
+
+  renderer = new CSS3DRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.getElementById("speakers").appendChild(renderer.domElement);
+
+  //RESIZE boilerplate code
+  window.addEventListener("resize", onWindowResize);
+
+  // init ends
+}
+
+//TWEENING CARDS STARTS
+function transform(targets, duration) {
+  TWEEN.removeAll();
+
+  for (let i = 0; i < objects.length; i++) {
+    const object = objects[i]; //INITIAL
+    const target = targets[i]; //FINAL
+
+    //TWEENING POSITION
+    new TWEEN.Tween(object.position)
+      .to(
+        {
+          x: target.position.x,
+          y: target.position.y,
+          z: target.position.z,
+        },
+        Math.random() * duration + duration
+      )
+      .easing(TWEEN.Easing.Exponential.Out)
+      .start();
+
+    //TWEENING ROTATION
+    new TWEEN.Tween(object.rotation)
+      .to(
+        {
+          x: target.rotation.x,
+          y: target.rotation.y,
+          z: target.rotation.z,
+        },
+        Math.random() * duration + duration
+      )
+      .easing(TWEEN.Easing.Exponential.Out)
+      .start();
+  }
+
+  new TWEEN.Tween(this)
+    .to({}, duration * 2)
+    .onUpdate(render)
+    .start();
+}
+//TWEENING CARD ENDS
+
+function animate() {
+  requestAnimationFrame(animate);
+  TWEEN.update(); //calling tween.update recursively for smoother tweening
+}
+
+var c = 0;
+
+var a = setInterval(function () {
+  if (SPEAKERS == "start") {
+    transform(targets.grid, 3500);
+  var b = setInterval(function () {
+    var n = Math.floor(Math.random() * 54);
+    // var n = c % objects.length;
+    var e = document.getElementsByClassName("element")[n];
+    e.className = e.className + " elementSeen";
+    setTimeout(function () {
+      e.className = e.className.split(" ")[0];
+    }, 3000);
+
+    new TWEEN.Tween(camera.position)
+      .to(
+        {
+          x: targets.grid[n].position.x,
+          y: targets.grid[n].position.y,
+          z: targets.grid[n].position.z + 250,
+        },
+        2000
+      )
+      .easing(TWEEN.Easing.Exponential.Out)
+      .onUpdate(render)
+      .start();
+    c += 1;
+  }, 3000);
+  console.log("bye");
+  clearInterval(a);
+} else {console.log(SPEAKERS);}
+}, 100);
+
+function render() {
+  renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  render();
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 gsap.to("#background", {
   duration: 2,
-  "background-image":
+  "background":
     "linear-gradient(315deg, #130f40 0%, #000000 50%),url(./assets/images/noise-fine.svg)",
 });
 
@@ -44,7 +44,7 @@ function textEffect1(textId) {
 
 textEffect1("heading");
 textEffect1("subHeading");
-
+// linear-gradient(147deg, rgb(0, 0, 0) 0%, #15062b 80%), url(./assets/images/noise.svg)
 var noise = "./assets/images/noise.svg";
 let tl2 = gsap.timeline({
   defaults: {
@@ -53,31 +53,16 @@ let tl2 = gsap.timeline({
   paused: true,
 });
 tl2
-  // .add(function () {
-  //   if (tl2.reversed()) {
-  //     transition.playing = false;
-  //   }
-  // })
-  .to("#d-flex", {
-    opacity: 0,
-    duration: 2,
-  })
-  .to(
-    "#second_",
-    {
-      height: "100%",
-      duration: 2,
-    },
-    "<+0.2"
-  )
-  .to(
-    "#first_",
-    {
-      height: "0vh",
-      duration: 2,
-    },
-    "<"
-  )
+.to(
+  "#background",
+  {
+    
+    "background":
+      'linear-gradient(147deg, rgb(0, 0, 0) 0%, rgb(50, 2, 41) 90%), url("./assets/images/noise.svg")',
+    duration: 3,
+  },
+  "<"
+)
   .to(
     "#about-head",
     {
@@ -121,16 +106,6 @@ tl2
     },
     "<"
   )
-  .to(
-    "#background",
-    {
-      "background-color": "#000000",
-      background:
-        "linear-gradient(147deg, rgb(0, 0, 0) 0%, rgb(50 2 41) 74%), url(./assets/images/noise.svg) rgb(0, 0, 0)",
-      duration: 1,
-    },
-    "<"
-  )
   ;
 
 let tl3 = gsap.timeline({
@@ -140,35 +115,10 @@ let tl3 = gsap.timeline({
   paused: true,
 });
 tl3
-  // .add(function () {
-  //   if (tl3.reversed()) {
-  //     transition.playing = false;
-  //   }
-  // })
-  .to("#second", {
-    height: "0",
-    duration: 2,
-  })
-  .to(
-    "#third",
-    {
-      height: "100%",
-      duration: 2,
-    },
-    "<"
-  )
-  .to(
-    "body",
-    {
-      height: "100%",
-      duration: 3,
-    },
-    "<+1"
-  )
-  .add(function () {
-    if (!tl3.reversed()) {
-      transition.playing = false;
-    }
+  .to("#background", {
+    "background":
+      "linear-gradient(147deg, rgb(0, 0, 0) 0%, rgb(2 54 56) 74%), url(./assets/images/noise.svg) rgb(0, 0, 0)",
+    duration: 3,
   });
 
 let tl4 = gsap.timeline({
@@ -177,6 +127,7 @@ let tl4 = gsap.timeline({
   },
   paused: true,
 });
+
 tl4
   // .add(function () {
   //   if (tl4.reversed()) {
@@ -210,9 +161,8 @@ tl4
     }
   });
 
-tl2.timeScale(3);
-tl3.timeScale(3);
-tl4.timeScale(3);
+tl2.timeScale(1.5);
+tl3.timeScale(1.5);
 
 // tl2.reverse(0);
 var currentPage = 1;
@@ -223,43 +173,6 @@ setTimeout(function () {
     e.style.setProperty("backdrop-filter", "blur(2.5px)");
   });
 }, 5000);
-
-// window.addEventListener("wheel", function (e) {
-//   console.log("step1");
-//   if (e.wheelDelta <= -1 && !transition.playing) {
-//     console.log("step2");
-//     transition.playing = true;
-//     if (currentPage == 1) {
-//       console.log("1 to 2");
-//       tl2.play();
-//       currentPage += 1;
-//     } else if (currentPage == 2) {
-//       console.log("2 to 3");
-//       tl3.play();
-//       currentPage += 1;
-//     } else if (currentPage == 3) {
-//       console.log("3 to 4");
-//       tl4.play();
-//       currentPage += 1;
-//     }
-//   }
-//   if (e.wheelDelta > 0) {
-//     console.log("step3");
-//     if (currentPage == 2 && !tl3.isActive() && !tl2.isActive()) {
-//       console.log("2 to 1");
-//       tl2.reverse();
-//       currentPage -= 1;
-//     } else if (currentPage == 3 && !tl4.isActive()  && !tl3.isActive()) {
-//       console.log("3 to 2");
-//       tl3.reverse();
-//       currentPage -= 1;
-//     } else if (currentPage == 4 && !tl4.isActive()  && !tl3.isActive()) {
-//       console.log("4 to 3");
-//       tl4.reverse();
-//       currentPage -= 1;
-//     }
-//   }
-// });
 
 //check for Navigation Timing API support
 if (window.performance) {
