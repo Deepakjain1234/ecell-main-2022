b4f7a00f408642ad64588744e2109be380205ca7
Ayushi-0803 <ayushimohta1@gmail.com>
2022-10-09 01:10:28 +0530

    final

--- esummit/JS-summit/app3.js
+++ esummit/JS-summit/app3.js
@@ -7,3 +7,4 @@ tl.fromTo('nav',{opacity:0},{opacity:1,duration:1});
 tl.fromTo('.big-text',{opacity:0},{opacity:1,duration:1},"-=1");
 tl.fromTo('.about-text',{opacity:0},{opacity:1,duration:1},"-=1");
 tl.fromTo('.heading',{opacity:0},{opacity:1,duration:1},"-=1");
+tl.fromTo('.esummit-img',{opacity:0},{opacity:1,duration:1},"-=1");
